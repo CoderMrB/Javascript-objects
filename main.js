24aaf21a33977ff50239e2e2f613051420ad2544
Christopher Webb <webbophone@gmail.com>
2023-03-16 12:18:07 +0000

    Files added and started.

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log(`Hi`)
+
+let myHouse = {stories:2, parking:false, bedrooms:2, garden:true, energyEfficiency:`D`}
+
+/* Create an object called myCar:
+My car is red, has 4 wheels, power steering, and 2 seats, and does 20 miles per gallon. */
+
+let myCar = {colour: `red`, wheels: 4, powerSteering: true, seats: 2, milesPerGallon: 20}
+
+let myFavouriteFilm = {title:`The Dark Knight`, releaseYear: 2008, leadActor: `Christian Bale`}
