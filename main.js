e84ce64bb448dc2f5ebb8ac4a4aa1417b5defe76
Chris Branson <chrisbranson@live.co.uk>
2023-03-16 12:30:23 +0000

    Completed second task

--- main.js
+++ main.js
@@ -8,3 +8,27 @@ My car is red, has 4 wheels, power steering, and 2 seats, and does 20 miles per 
 let myCar = {colour: `red`, wheels: 4, powerSteering: true, seats: 2, milesPerGallon: 20}
 
 let myFavouriteFilm = {title:`The Dark Knight`, releaseYear: 2008, leadActor: `Christian Bale`}
+
+let person = {
+  name: "Ben",
+  likesChocolate: true,
+};
+
+console.log(person.likesChocolate)
+
+if (person.likesChocolate === true){
+    console.log(`${person.name} loves chocolate`)
+}
+    else{console.log(`${person.name} hates chocolate`)
+
+    };
+
+person.name = `Chris`;
+person.likesChocolate = false;
+
+if (person.likesChocolate === true){
+    console.log(`${person.name} loves chocolate`)
+}
+    else{console.log(`${person.name} hates chocolate`)
+
+    };
