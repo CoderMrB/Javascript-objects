8eb05366ac9e2bfc55bc3ac2f475c8b8352a8287
Chris Branson <chrisbranson@live.co.uk>
2023-03-28 20:40:12 +0100

    More practice

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-console.log(`Hi`)
+const myHouse = {
+    stories: 2,
+    parking: false,
+    bedrooms: 2,
+    garden: true,
+    energyRating: "D",
+};
 
-let myHouse = {stories:2, parking:false, bedrooms:2, garden:true, energyEfficiency:`D`}
+const myCar = {
+    colour: "red",
+    wheels: 4,
+    powerSteering: true,
+    seats: 2,
+    mpg: 20,    
+};
 
-/* Create an object called myCar:
-My car is red, has 4 wheels, power steering, and 2 seats, and does 20 miles per gallon. */
-
-let myCar = {colour: `red`, wheels: 4, powerSteering: true, seats: 2, milesPerGallon: 20}
-
-let myFavouriteFilm = {title:`The Dark Knight`, releaseYear: 2008, leadActor: `Christian Bale`}
+const myFavouriteFilm = {
+    title: "The Dark Knight",
+    releaseYear: 2008,
+    leadActor: "Christian Bale",
+};
 
 let person = {
-  name: "Ben",
+  name: "John",
   likesChocolate: true,
 };
 
-console.log(person.likesChocolate)
+console.log(person.likesChocolate);
 
-if (person.likesChocolate === true){
-    console.log(`${person.name} loves chocolate`)
+if (person.likesChocolate === false){
+    console.log(`${person.name} likes chocolate`)
+}else{
+    console.log(`${person.name} hates chocolate`)
 }
-    else{console.log(`${person.name} hates chocolate`)
-
-    };
-
-person.name = `Chris`;
-person.likesChocolate = false;
-
-if (person.likesChocolate === true){
-    console.log(`${person.name} loves chocolate`)
-}
-    else{
-        console.log(`${person.name} hates chocolate`)
-    };
 
 let bensPhrases = {
   greeting: "Well hello there!",
@@ -39,15 +39,8 @@ let bensPhrases = {
   smallTalk: "Warm today, isn't it...",
 };
 
-let desiredPleasantry = bensPhrases[`greeting`];
+let desiredPleasantry = (bensPhrases["farewell"]);
 
-console.log(bensPhrases[`smallTalk`]);
+console.log(bensPhrases["smallTalk"])
 
-console.log(desiredPleasantry);
-
-desiredPleasantry = `Sup!`;
-
-let array = ["Gratin", "Fish fingers", "Cheese roll", "Banana"]
-console.log(array)
-
-console.log(array[1])
+console.log(desiredPleasantry)
