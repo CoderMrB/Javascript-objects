1d28eda5721baa124f2355be4eddb0bddefd72bd
Chris Branson <chrisbranson@live.co.uk>
2023-03-28 21:00:37 +0100

    Doing fun things with objects, functions, prompts and alerts.

--- main.js
+++ main.js
@@ -44,3 +44,42 @@ let desiredPleasantry = (bensPhrases["farewell"]);
 console.log(bensPhrases["smallTalk"])
 
 console.log(desiredPleasantry)
+
+let me = {
+  firstName: "Chris",
+  lastName: "Branson",
+  isBootcamper: false,
+};
+
+me.isBootcamper = true;
+console.log(`Is ${me.firstName} a bootcamper? Yes, that is ${me["isBootcamper"]}!`)
+
+let communication = {
+  success: true,
+  payload: {
+    message: {
+      text: "Please send biscuits!",
+      priority: "URGENT",
+    },
+  },
+};
+
+let message = (communication["payload"].message.text)
+
+
+if ((communication["payload"].message.priority) === "URGENT"){
+    console.log(communication["payload"].message.text);
+}
+
+let yourDetails = {
+    yourName: "",
+    yourAge: ""
+}
+
+function whoAreYou(name, age){
+    yourDetails.yourName = name;
+    yourDetails["yourAge"] = age;
+    alert(`So lovely to meet you ${yourDetails.yourName}! You are never ${yourDetails["yourAge"]}!? You don't look a day over ${(Math.floor(yourDetails["yourAge"]/2))} .`)
+}
+
+whoAreYou(prompt("Hi there, what's your name?"), prompt("Lovely. And how old are you?"))
